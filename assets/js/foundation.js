31fc9cbc5050701f170c1859ecc538aaf786ce0e
Andy Cochran <acochran@council.nyc.gov>
2016-02-29 16:42:27 -0500

    add WhatInput to npm dependencies to makes sure it uses v2

--- assets/js/foundation.js
+++ assets/js/foundation.js
@@ -1,18 +1,7 @@
 'use strict';
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+window.whatInput = function () {
 
-;(function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define([], function () {
-      return factory();
-    });
-  } else if ((typeof exports === 'undefined' ? 'undefined' : _typeof(exports)) === 'object') {
-    module.exports = factory();
-  } else {
-    root.whatInput = factory();
-  }
-})(undefined, function () {
   'use strict';
 
   /*
@@ -26,7 +15,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
   var activeKeys = [];
 
   // cache document.body
-  var body = document.body;
+  var body;
 
   // boolean: true if touch buffer timer is running
   var buffer = false;
@@ -34,26 +23,40 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
   // the last used input type
   var currentInput = null;
 
-  // array of form elements that take keyboard input
-  var formInputs = ['input', 'select', 'textarea'];
+  // `input` types that don't accept text
+  var nonTypingInputs = ['button', 'checkbox', 'file', 'image', 'radio', 'reset', 'submit'];
 
-  // user-set flag to allow typing in form fields to be recorded
-  var formTyping = body.hasAttribute('data-whatinput-formtyping');
+  // detect version of mouse wheel event to use
+  // via https://developer.mozilla.org/en-US/docs/Web/Events/wheel
+  var mouseWheel = detectWheel();
+
+  // list of modifier keys commonly used with the mouse and
+  // can be safely ignored to prevent false keyboard detection
+  var ignoreMap = [16, // shift
+  17, // control
+  18 // alt
+  ];
 
   // mapping of events to input types
   var inputMap = {
     'keydown': 'keyboard',
+    'keyup': 'keyboard',
     'mousedown': 'mouse',
-    'mouseenter': 'mouse',
-    'touchstart': 'touch',
+    'mousemove': 'mouse',
+    'MSPointerDown': 'pointer',
+    'MSPointerMove': 'pointer',
     'pointerdown': 'pointer',
-    'MSPointerDown': 'pointer'
+    'pointermove': 'pointer',
+    'touchstart': 'touch'
   };
+
+  // add correct mouse wheel event mapping to `inputMap`
+  inputMap[detectWheel()] = 'mouse';
 
   // array of all used input types
   var inputTypes = [];
 
-  // mapping of key codes to common name
+  // mapping of key codes to a common name
   var keyMap = {
     9: 'tab',
     13: 'enter',
@@ -82,31 +85,43 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
     ---------------
   */
 
-  function bufferInput(event) {
-    clearTimeout(timer);
-
+  // allows events that are also triggered to be filtered out for `touchstart`
+  function eventBuffer() {
+    clearTimer();
     setInput(event);
 
     buffer = true;
-    timer = setTimeout(function () {
+    timer = window.setTimeout(function () {
       buffer = false;
-    }, 1000);
+    }, 650);
   }
 
-  function immediateInput(event) {
+  function bufferedEvent(event) {
     if (!buffer) setInput(event);
+  }
+
+  function unBufferedEvent(event) {
+    clearTimer();
+    setInput(event);
+  }
+
+  function clearTimer() {
+    window.clearTimeout(timer);
   }
 
   function setInput(event) {
     var eventKey = key(event);
-    var eventTarget = target(event);
     var value = inputMap[event.type];
     if (value === 'pointer') value = pointerType(event);
 
+    // don't do anything if the value matches the input type already set
     if (currentInput !== value) {
-      if (
-      // only if the user flag isn't set
-      !formTyping &&
+      var eventTarget = target(event);
+      var eventTargetNode = eventTarget.nodeName.toLowerCase();
+      var eventTargetType = eventTargetNode === 'input' ? eventTarget.getAttribute('type') : null;
+
+      if ( // only if the user flag to allow typing in form fields isn't set
+      !body.hasAttribute('data-whatinput-formtyping') &&
 
       // only if currentInput has a value
       currentInput &&
@@ -115,20 +130,26 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
       value === 'keyboard' &&
 
       // not if the key is `TAB`
-      keyMap[eventKey] !== 'tab' &&
+      keyMap[eventKey] !== 'tab' && (
 
-      // only if the target is one of the elements in `formInputs`
-      formInputs.indexOf(eventTarget.nodeName.toLowerCase()) >= 0) {
-        // ignore keyboard typing on form elements
+      // only if the target is a form input that accepts text
+      eventTargetNode === 'textarea' || eventTargetNode === 'select' || eventTargetNode === 'input' && nonTypingInputs.indexOf(eventTargetType) < 0) ||
+      // ignore modifier keys
+      ignoreMap.indexOf(eventKey) > -1) {
+        // ignore keyboard typing
       } else {
-          currentInput = value;
-          body.setAttribute('data-whatinput', currentInput);
-
-          if (inputTypes.indexOf(currentInput) === -1) inputTypes.push(currentInput);
+          switchInput(value);
         }
     }
 
     if (value === 'keyboard') logKeys(eventKey);
+  }
+
+  function switchInput(string) {
+    currentInput = string;
+    body.setAttribute('data-whatinput', currentInput);
+
+    if (inputTypes.indexOf(currentInput) === -1) inputTypes.push(currentInput);
   }
 
   function key(event) {
@@ -140,7 +161,11 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
   }
 
   function pointerType(event) {
-    return typeof event.pointerType === 'number' ? pointerMap[event.pointerType] : event.pointerType;
+    if (typeof event.pointerType === 'number') {
+      return pointerMap[event.pointerType];
+    } else {
+      return event.pointerType === 'pen' ? 'touch' : event.pointerType; // treat pen like touch
+    }
   }
 
   // keyboard logging
@@ -156,27 +181,49 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
   }
 
   function bindEvents() {
+    body = document.body;
 
-    // pointer/mouse
-    var mouseEvent = 'mousedown';
-
+    // pointer events (mouse, pen, touch)
     if (window.PointerEvent) {
-      mouseEvent = 'pointerdown';
+      body.addEventListener('pointerdown', bufferedEvent);
+      body.addEventListener('pointermove', bufferedEvent);
     } else if (window.MSPointerEvent) {
-      mouseEvent = 'MSPointerDown';
+      body.addEventListener('MSPointerDown', bufferedEvent);
+      body.addEventListener('MSPointerMove', bufferedEvent);
+    } else {
+
+      // mouse events
+      body.addEventListener('mousedown', bufferedEvent);
+      body.addEventListener('mousemove', bufferedEvent);
+
+      // touch events
+      if ('ontouchstart' in window) {
+        body.addEventListener('touchstart', eventBuffer);
+      }
     }
 
-    body.addEventListener(mouseEvent, immediateInput);
-    body.addEventListener('mouseenter', immediateInput);
+    // mouse wheel
+    body.addEventListener(mouseWheel, bufferedEvent);
 
-    // touch
-    if ('ontouchstart' in window) {
-      body.addEventListener('touchstart', bufferInput);
-    }
-
-    // keyboard
-    body.addEventListener('keydown', immediateInput);
+    // keyboard events
+    body.addEventListener('keydown', unBufferedEvent);
+    body.addEventListener('keyup', unBufferedEvent);
     document.addEventListener('keyup', unLogKeys);
+  }
+
+  /*
+    ---------------
+    utilities
+    ---------------
+  */
+
+  // detect version of mouse wheel event to use
+  // via https://developer.mozilla.org/en-US/docs/Web/Events/wheel
+  function detectWheel() {
+    return mouseWheel = 'onwheel' in document.createElement('div') ? 'wheel' : // Modern browsers support "wheel"
+
+    document.onmousewheel !== undefined ? 'mousewheel' : // Webkit and IE support at least "mousewheel"
+    'DOMMouseScroll'; // let's assume that remaining browsers are older Firefox
   }
 
   /*
@@ -188,7 +235,15 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
   */
 
   if ('addEventListener' in window && Array.prototype.indexOf) {
-    bindEvents();
+
+    // if the dom is already ready already (script was placed at bottom of <body>)
+    if (document.body) {
+      bindEvents();
+
+      // otherwise wait for the dom to load (script was placed in the <head>)
+    } else {
+        document.addEventListener('DOMContentLoaded', bindEvents);
+      }
   }
 
   /*
@@ -215,9 +270,9 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
     },
 
     // accepts string: manually set the input type
-    set: setInput
+    set: switchInput
   };
-});
+}();
 'use strict';
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
